5126a3bca2bde4043aa51bb1a32076f6888d5491
Rusi Li <rusili56@gmail.com>
2017-01-31 15:56:00 -0500

    Working youtube api

---
+++ app/src/main/java/nyc/c4q/rusili/audiotube/Youtube/YouTubeFailureRecoveryActivity.java
@@ -0,0 +1,42 @@
+package nyc.c4q.rusili.audiotube.Youtube;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+import nyc.c4q.rusili.audiotube.R;
+
+/**
+ * An abstract activity which deals with recovering from errors which may occur during API
+ * initialization, but can be corrected through user action.
+ */
+public abstract class YouTubeFailureRecoveryActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    @Override
+    public void onInitializationFailure (YouTubePlayer.Provider provider,
+                                         YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(getString(R.string.error_player), errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(String.valueOf(R.string.developer_key), this);
+        }
+    }
+
+    protected abstract YouTubePlayer.Provider getYouTubePlayerProvider ();
+
+}
