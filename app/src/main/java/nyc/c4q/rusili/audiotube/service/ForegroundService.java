cac555aa42661ade0e5af1711105ff76b04c6449
Rusi Li <rusili56@gmail.com>
2017-02-16 16:37:59 -0500

    Custom notification buttons work

--- app/src/main/java/nyc/c4q/rusili/audiotube/service/ForegroundService.java
+++ app/src/main/java/nyc/c4q/rusili/audiotube/service/ForegroundService.java
@@ -8,34 +8,39 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import com.google.android.youtube.player.YouTubePlayer;
 
-import nyc.c4q.rusili.audiotube.other.Constants;
+import nyc.c4q.rusili.audiotube.R;
 import nyc.c4q.rusili.audiotube.activities.ActivityMain;
+import nyc.c4q.rusili.audiotube.other.Constants;
 import nyc.c4q.rusili.audiotube.youtube.MyYoutubePlayer;
 
 public class ForegroundService extends Service {
     private String LOG_TAG = "ForegroundService: ";
     private MyYoutubePlayer myYoutubePlayer;
     private YouTubePlayer youTubePlayer;
+    private RemoteViews smallView;
 
     @Override
     public void onCreate () {
         super.onCreate();
         myYoutubePlayer = ActivityMain.myYoutubePlayer;
-        youTubePlayer = myYoutubePlayer.getYouTubePlayer();
     }
 
     @Override
     public int onStartCommand (Intent intent, int flags, int startId) {
+        youTubePlayer = myYoutubePlayer.getYouTubePlayer();
         String url = intent.getStringExtra("url");
         Log.i("url: ", " " + url);
+        url = "9JJmHYZQci4";
         youTubePlayer.loadVideo(url);
 
         if (intent.getAction().equals(Constants.ACTION.STARTFOREGROUND_ACTION)) {
             Log.i(LOG_TAG, "Received Start Foreground Intent ");
 
+            smallView = new RemoteViews(getPackageName(), R.layout.notification_bar);
             Intent notificationIntent = new Intent(this, ActivityMain.class);
             notificationIntent.setAction(Constants.ACTION.MAIN_ACTION);
             notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
@@ -48,12 +53,14 @@ public class ForegroundService extends Service {
             pauseIntent.setAction(Constants.ACTION.PAUSE_ACTION);
             PendingIntent ppauseIntent = PendingIntent.getService(this, 0,
                     pauseIntent, 0);
+            smallView.setOnClickPendingIntent(R.id.notification_button_pause, ppauseIntent);
 
             // Play
             Intent playIntent = new Intent(this, ForegroundService.class);
             playIntent.setAction(Constants.ACTION.PLAY_ACTION);
             PendingIntent pplayIntent = PendingIntent.getService(this, 0,
                     playIntent, 0);
+            smallView.setOnClickPendingIntent(R.id.notification_button_play, pplayIntent);
 
             // Next
             Intent nextIntent = new Intent(this, ForegroundService.class);
@@ -66,12 +73,8 @@ public class ForegroundService extends Service {
                     .setSmallIcon(android.R.drawable.btn_radio)
                     .setContentIntent(pendingIntent)
                     .setOngoing(true)
-                    .addAction(android.R.drawable.ic_media_previous,
-                            "Pause", ppauseIntent)
-                    .addAction(android.R.drawable.ic_media_play, "Play",
-                            pplayIntent)
-                    .addAction(android.R.drawable.ic_media_next, "Next",
-                            pnextIntent).build();
+                    .setContent(smallView).build();
+
             startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
                     notification);
         } else if (intent.getAction().equals(Constants.ACTION.PAUSE_ACTION)) {
